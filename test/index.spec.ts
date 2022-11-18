fb2cf9d3134195947f1597e308d52cab5c9df77c
manuel <mlondono@q10.com>
2022-11-18 13:00:05 -0500

    first commit

---
+++ test/index.spec.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest"
+
+// let add = 
+
+describe('add', () => { 
+    it('should be a function', () => {
+        expect(add).toBeTypeOf('function')
+    })
+ })
